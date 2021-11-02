b8f5e051280cf89a5edad13ff34cb390eae3464f
kaiofelixdeoliveira <kaio.felix.oliveira.mail@gmail.com>
2021-11-02 16:42:10 -0300

    init

---
+++ src/main/java/com/azure/app/AzureRepoExampleApplication.java
@@ -0,0 +1,14 @@
+package com.azure.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureRepoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureRepoExampleApplication.class, args);
+	}
+	
+
+}
---
+++ src/test/java/com/azure/app/AzureRepoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureRepoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
